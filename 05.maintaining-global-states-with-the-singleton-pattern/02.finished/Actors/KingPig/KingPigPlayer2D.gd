99d83d74aba60e3aac544a5c4412783385cf0511
Henrique Campos <henriiquecampos@gmail.com>
2024-05-28 12:18:59 -0300

    Add Chapter 5 fininished version

---
+++ 05.maintaining-global-states-with-the-singleton-pattern/02.finished/Actors/KingPig/KingPigPlayer2D.gd
@@ -0,0 +1,72 @@
+extends Player2D
+
+signal died
+
+@export var lives = 3
+
+@onready var animated_sprites = $Sprites/AnimatedSprite2D
+@onready var sprites = $Sprites
+@onready var current_lives = lives
+@onready var hit_box = $HitArea2D
+@onready var animation_player = $AnimationPlayer
+
+var fall_speed = 0.0
+
+func _physics_process(delta):
+	super(delta)
+	
+	if direction > 0:
+		sprites.scale.x = 1
+		hit_box.scale.x = 1
+	elif direction < 0:
+		sprites.scale.x = -1
+		hit_box.scale.x = -1
+	
+	var animation = animated_sprites.animation
+	if is_on_floor():
+		if not direction == 0.0:
+			animation = "run"
+		else:
+			animation = "idle"
+	else:
+		if velocity.y >= 0.0:
+			animation = "fall"
+			fall_speed = velocity.y
+		else:
+			animation = "jump"
+		
+	if fall_speed > 0.0 and is_on_floor():
+		animation = "ground"
+	animated_sprites.play(animation)
+
+
+func _unhandled_input(event):
+	if event.is_action("attack"):
+		if event.is_pressed():
+			if is_on_floor():
+				animation_player.play("attack")
+	super(event)
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprites.animation == "ground":
+		fall_speed = 0.0
+		animated_sprites.play("idle")
+	elif animated_sprites.animation == "hit":
+		set_physics_process(true)
+		set_process_unhandled_input(true)
+		animated_sprites.play("idle")
+
+
+func _on_hurt_area_2d_hurt(damage):
+	set_physics_process(false)
+	set_process_unhandled_input(false)
+	direction = 0
+	current_lives -= damage
+	if current_lives < 1:
+		animated_sprites.play("dead")
+		animation_player.play("dead")
+		await animation_player.animation_finished
+		died.emit()
+	else:
+		animated_sprites.play("hit")
---
+++ 05.maintaining-global-states-with-the-singleton-pattern/02.finished/Objects/Diamond/Diamond.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+@export var pop_label_scene = preload("res://Interface/PopLabel/PopLabel.tscn")
+@export var score = 175
+
+
+func _on_interactive_area_2d_interaction_available():
+	var pop_label = pop_label_scene.instantiate()
+	find_parent("Level").find_child("PopLabels").add_child(pop_label)
+	pop_label.pop(str(score), global_position)
+	Score.current += score
+	find_parent("Game").update_score_label()
+	queue_free()
